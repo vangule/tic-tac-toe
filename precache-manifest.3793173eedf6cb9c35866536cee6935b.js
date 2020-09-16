e29462574d13b340b1744537193fb026ead0198d
Chandrakant <csvangule.1999@gmail.com>
2020-09-16 16:09:26 +0530

    Updates

---
+++ precache-manifest.3793173eedf6cb9c35866536cee6935b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f26de9472e5394593fcd094ad2988e",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "949b13f7dba303589a6c",
+    "url": "/tic-tac-toe/static/css/main.fb7d7479.chunk.css"
+  },
+  {
+    "revision": "97fe81171580e18c4fea",
+    "url": "/tic-tac-toe/static/js/2.342420e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.342420e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "949b13f7dba303589a6c",
+    "url": "/tic-tac-toe/static/js/main.6c93ab8b.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);

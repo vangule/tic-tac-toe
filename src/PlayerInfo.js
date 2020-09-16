8037d759ee74c00758d7928a337463ab0237a73e
Chandrakant <csvangule.1999@gmail.com>
2020-09-16 16:50:22 +0530

    made changes in grid module

--- src/PlayerInfo.js
+++ src/PlayerInfo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function PlayerInfo(props){
-  const element = props.winner ? <h1>Winner: {props.winner}</h1> :   <h2>Turn: {props.turn}</h2>
+  const element = props.winner ? <h1>Winner: {props.winner}</h1> :   <h2>Turn: &nbsp; {props.turn}</h2>
   
   return element;
 }
